e4ea1c31630c4d0145e85cf2cd8146e650b5ca2b
Siba Kumar Panda <51659486+Sibakumarpanda@users.noreply.github.com>
2021-04-10 17:12:20 +0530

    Create hello_perl.pl
    How to use comment line in Perl
    How to define the variable (it can be a numeric or string variable) in Perl ??

---
+++ hello_perl.pl
@@ -0,0 +1,16 @@
+#!/usr/local/bin/perl
+print "Hello World\n";
+print "Welcome to learn PERL\n";
+# This is the comment line
+# Want to define the variable (it can be a numeric or string variable)in perl?? How it will be
+$v1="Hello";
+print $v1;
+print"\n";
+# doing Arithmetic operation
+$v2=4;
+print $v2;
+$v3=10;
+print"\n";
+$v4=$v2 +$v3;
+print $v4;
+
